fb785d787c5ac5644e60a10b7d23d79a6c1c5177
hoge <hina_lala@icloud.com>
2020-09-23 13:54:20 +0900

    add index.vue

---
+++ pages/index.vue
@@ -0,0 +1,871 @@
+<template>
+  <div>
+    
+    
+
+    <header class="header">
+      <div class="contents-wrap-l">
+        <div class="header-top">
+          <div class="left">
+            <a href="http://www.sanch-gonda.jp">
+            <img src="http://www.sanch-gondo.jp/cms/wp-content/themes/sanch/img/logo-h.svg" ></a>
+          </div>
+        </div>
+        </div>
+
+
+
+    </header>
+
+      
+            <div class="site-heading">
+              <img src="http://www.sanch-gondo.jp/cms/wp-content/themes/sanch/img/logo-v-w.svg" alt="SANCH">
+              </div>
+              
+            
+          
+    
+    
+
+  
+    <!-- Main Content -->
+    <div class="container">
+          
+          <div class="post-preview">
+              <p>News 2020.05.18<br>
+              <a href="http://www.sanch-gondo.jp/news/140">
+           　　　　　Webサイトをリニューアルしました</a></p>
+              <h3 class="post-title">
+                What's
+              </h3>
+              <h2 class="post-subtitle">
+                SANCH
+              </h2>
+             <img src="ichigo.png">
+             <img src="ichigo2.png">
+             <img src="ichigo3.png">
+             <img src="ichigo4.png">
+             <img src="blueberry.png">
+             <img src="blueberry6.png">
+             <img src="blueberry7.png">
+             <img src="blueberry8.png">
+
+
+              
+            </a>
+            <p class="post-meta">長野県長野市権堂町にある、<br>
+                  フルーツとパンでサンチ（産地）を味わえるお店。 <br>
+                  
+                  こだわりの食パン、ボリューミーなサンドイッチ、<br>
+                  旬の果物をふんだんに使ったパフェなど、<br>
+                  フルーツとパンのある毎日をお届けします。
+                  <a href="/about"><br>Concept<br>→</a>
+            </p>
+            
+            
+               
+          </div>
+          <hr>
+          <div class="post-preview">
+            <a href="post.html">
+              
+              <p>Bread</p>
+              <h2 class="post-title">
+                耳まですべてが柔らかい。<br>
+                  妥協なき食パン。
+              </h2>
+            </a>
+            <p class="shokupan">サンチの食パンは、信州生まれの小麦粉や牛乳を使った、甘みとミルクの風味に包まれた、柔らかくて優しい食パンです。大切な人に教えたい、自慢したい食パンを目指しています。</p>
+            <p class="more">More→</p>
+            <img src="syokupan.png" width="100%" height="auto">
+
+            <li>
+            <div class="fruit">
+            <p>Fruit</p>
+            <h2>果実の魅力を<br>最大限に引き出すために。</h2>
+            <p>食べごろ、食べ方、適した加工。フルーツにみずみずしい命を吹き込んだ農家の方々と語らうことで、食パンの美味しさとフルーツの味わいをお互いが引き立てるサンドイッチが完成します。</p>
+            <p>More→</p></div></li>
+           
+          
+
+
+            <p>Menu</p>
+            <h2>サンドイッチにストーリーを。</h2>
+            <p>見た目のワクワク感と、食べた時の満足感、納得感。お客様の感情をサンドイッチで揺さぶりたいと思っています。そんなサンドイッチを作るために、私たちはいつだってストーリーを考えています。</p>
+            <p>More→</p>
+            <img src="fruitsand.pn" alt class=sand1>
+            <img src="chocobananasand.pn" alt class="sand2">
+            <img src="yasaisand.pn" alt class="sand3">
+
+
+
+
+
+            
+           
+              <div class="interview">
+              
+              <h2>Interview</h2>
+             <p>サンチのはなし</p>
+            
+            
+
+              <ul class="interview">
+              
+             
+              <div class="ichigo">
+                <div>
+                <h2>#04</h2>
+                </div>
+                <h3>いちごのはなし</h3>
+                <p>いちごの旬の季節 春！満開な4月中旬、須坂市の小林いちご園の小林さんにサンチのはなしを伺いに。</p>
+              </div>
+             
+             <div class="budou">
+               <div>
+                <h2>#03</h2>
+                </div>
+            
+
+
+                <h3>ぶどうのはなし</h3>
+                <p>秋も深まる9月。 サンチで使用する葡萄を作っていらっしゃる ぶどう農家さん・千曲市の昇陽園さんに旬を迎えたぶどうのサンチのはなしを伺いに行ってきました。</p>
+            
+              </div>
+              
+              <div class="komugi">
+                <div>
+                <h2>#02</h2></div>
+                
+              
+                <h3>小麦のはなし</h3>
+                <p>早朝、前日までの雨が上がり、むんむんとした空気と暑い夏の日差しが照り付けるなか、浅間山をのぞむ丘の上の小麦畑で長野県産小麦の収穫見学と、柄木田製粉さんの宮崎製造本部長と北島さんにサンチのはなしをお聞きしてきました。</p>
+              </div>
+               
+           </ul>
+           
+            <div class="more0">
+            <p>More→</p>
+
+        <ul class="event1">
+            <div class="eventimage"> </div>
+            <h2>Event</h2>
+            <p>イベント</p>
+            <p class=meta>サンドイッチの作り方教室や試食会など、様々なイベントを企画しています。お楽しみに。</p>
+            
+          <div class=event>
+          <div class=event01> </div>
+          <div class=event02> </div>
+          <div class=event03> </div>
+          </div>
+
+        </ul>
+            <p>More→</p>
+        
+
+            <div class="info">
+            <h2>Information</h2>
+            <p>店舗情報</p>
+            </p></div>
+            <img src="gaikan.jpg" width="100%">
+            
+            <p>営業時間</p>
+            <h2>10:00 - 17:00</h2><span>(L.O 16:30)</span>
+            <p>LUNCH 11:00 - 15:00(L.O 14:30)
+            <br>一部の商品を除きテイクアウトOK
+            <br>店内無料Wi-Fi / カウンター席はコンセント有
+
+            <p>ご予約・お問い合わせ</p>
+            <h2>026-237-2988</h2>
+            <div class="mail">
+            メールでご注文</div>
+           <div class="p">
+            <p>・ランチ時11:00～15:00はお電話がつながりにくい場合があります。</p>
+            <p>・代金は商品受け取りの際に店頭にてお支払いください。</p>
+            <p>・お会計の際に混みあう場合は順番でご案内となります。</p>
+           ・季節・材料の仕入れ状況によりメニュー変更する場合がございます。</div>
+            <hr>
+            お席のご予約<br>
+            2名様〜(6名様以上は要相談)
+            <hr>
+            サンドイッチのお取り置き<br>
+            15個まで  当日11:00までに要予約<br>
+            15個以上  3日前17:00までに要予約
+            <hr>
+            大口注文配達<br>
+            配達条件  3,000円以上<br>
+            前日昼までのご予約
+            配達料金  無料
+            配達地域  店舗から10km圏内当日11:00までに要予約
+            <hr>
+            催事販売のご相談<br>
+            イベントをご検討中の企業・法人様<br>
+            SANCHは数多くの百貨店催事、イベント催事に出店させて頂いております。
+            焼きたての食パンでお作りしたこだわりのサンドイッチをイベント会場までお持ちします！！ 当スタッフが販売に伺うできますので、お気軽にご相談くださいませ。
+            <hr>
+          <div class="tennai">
+          <img src="syokupan.tennai.jpg">
+          <img src="tooster.jpg">
+          <img src="tennai.jpg">
+          <img src="wifi.tennai.jpg">
+          </div>
+
+          <img src="map.svg" >
+
+            〒380-0833 長野県長野市権堂町2201 権堂イーストプラザ1F<br>
+            <p>TEL/FAX  026-237-2988<br></p>
+            長野電鉄『権堂駅』から田町方面へ、徒歩1分。
+            長野グランドシネマズ様隣接の長電権堂パーキングから徒歩すぐ。
+
+           
+            <div class="google">
+              <a href="https://www.google.com/maps/place/%E3%83%95%E3%83%AB%E3%83%BC%E3%83%84%EF%BC%86%E3%83%96%E3%83%AC%E3%83%83%E3%83%89+%E3%82%B5%E3%83%B3%E3%83%81/@36.6535246,138.1923893,15z/data=!4m5!3m4!1s0x0:0xd27998603a8ca5cf!8m2!3d36.6535246!4d138.1923893">Google Mapで見る</a></div>
+            
+            <div class="car">
+            <p>指定駐車場：長電権堂パーキング（587台）</p>
+
+            長電権堂パーキングご利用のお客様に、<br>
+            駐車料金のサービスを行っております。<br>
+            お食事・テイクアウト 1,200円以上お買い上げで1時間無料駐車券<br>
+            ※サービス時間を超過した場合の超過料金は現金でお支払いください。
+            </div>
+
+           
+           <img src="nagano.station.png" width="100%">
+            <div class="midori">
+            <p>SANCH 長野駅ビルMIDORI店</p><small>TAKE OUT ONLY</small>
+            <p>MIDORI長野店１F　Midoriマルシェ内</p></div>
+            <div class="google2">
+            <a href="https://www.google.com/maps/place/MIDORI+%E9%95%B7%E9%87%8E/@36.6441114,138.1863431,17z/data=!3m1!5s0x601d86928e95f4b7:0x486cece4198ab280!4m5!3m4!1s0x601d8692973fffff:0xacda1a8b7823f1e8!8m2!3d36.6436514!4d138.1887931">Google Mapで見る</a></div>
+            <div class="nagano">
+            <p>長野県長野市南千歳1-22-6（長野駅直結）<br>
+            営業時間  10:00～20:00
+            <p>MIDORI長野限定商品も登場します。<br>
+            長野駅ご利用の際は是非お越しください。<br>
+            コッペパンも販売中です。</p></div>
+
+            <div class="sns">
+            <h2>Facebook</h2>
+            <a href="https://www.facebook.com/sanch.gondo/"></a>
+            <div class="facebook">
+            <p>フルーツ&ブレットサンチ</p>
+
+            </div>
+
+
+
+            <p>Instagram</p><br>@sanch.gondo
+            <a href="https://www.instagram.com/sanch.gondo/"></a>
+
+            </div>
+
+
+          
+        </div>
+      </div>
+      </div>
+      
+  
+    <hr>
+
+    
+       </div>
+  </div>   
+  
+</template>
+
+<script>
+  export default {
+  
+  }
+</script>
+
+<style>
+
+
+@media(max-width: 1100px){
+.header{
+
+  height: 4rem;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    width: 100%;
+    background: #FFF;
+}
+}
+.header
+{
+padding: 1em 0;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
+    z-index: 999;
+    background: #FFF;
+
+}
+
+.left img
+{
+  width: 100px;
+  text-align: left;
+  
+}
+
+
+
+
+
+  a {
+    color:#212529;
+    transition: all 0.2s;
+  }
+
+  #mainNav {
+    position: absolute;
+    border-bottom: 1px solid #e9ecef;
+    
+    font-family: Arial, Helvetica, sans-serif
+  }
+
+
+
+
+  header.masthead {
+    
+    background: no-repeat center center;
+    
+    background-attachment: scroll;
+    position: relative;
+    background-size: cover;
+    text-align: center;
+    
+    color: white;
+  }
+
+  header.masthead .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    padding: 200px 0 100px;
+    
+  }
+
+  footer {
+    padding: 50px 0 65px;
+    text-align: center;
+    
+
+  }
+  
+
+
+
+.site-heading 
+{
+  background-image: url("/mainvisual.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 0 0 40px 40px;
+  display: block;
+  height: 400px;
+  text-align: center;
+}
+
+  
+ .site-heading img
+ {
+   width: 250px;
+   height: 250px;
+   text-align: center;
+   margin-top: 100px;
+
+ }
+ 
+
+.post-preview p
+{
+margin-top: 60px;
+margin-bottom:60px;
+}
+
+.post html
+{
+  text-align: left;
+  margin-bottom: 20px;
+  color: #000000
+}
+  
+h3.post-title
+
+{text-align: center;
+
+
+
+}
+
+h2.post-subtitle
+{
+  text-align: center;
+  font-size: 60px;
+  }
+
+p.post-meta
+{text-align: center}
+
+p.concept
+{
+  text-align: center;
+  padding-top: 20px;
+  }
+
+p.shokupan 
+{
+  padding-top: 20px;
+  
+}
+
+
+
+
+p.more 
+{
+  font-size: 30px;
+  
+}
+
+
+div.fruit
+{
+  margin: 0 auto;
+  width: 90%;
+  max-width: 1350px;
+}
+
+
+.sand1
+{
+    position: absolute;
+    width: 40%;
+    top: 40%;
+    right: 25%;
+    margin: auto;
+    z-index: 3;
+    max-width: 600px;
+    
+
+}
+
+@media(max-width:900px){
+.sand1{
+top: -8vw;
+}
+}
+
+
+
+
+
+.sand2
+{
+    position: absolute;
+    width: 20%;
+    left: 70%;
+    top: 30%;
+    margin: auto;
+    max-width: 500px;
+}
+.sand3
+{
+position: absolute;
+    width: 35%;
+    left: 55%;
+    top: 46%;
+    margin: auto;
+    z-index: 2;
+ 
+}
+
+
+li
+{
+  background-image: url("/fruit.png");
+  background-position-x: 25%;
+  background-size: 300% auto;
+  background-repeat: no-repeat;
+  
+  
+  
+}
+
+
+
+
+.ichigo div h2
+{
+    background-image: url("/ichigogarii.png");
+    border-radius: 50%;
+    width: 100px;
+    height: 100px;
+    background-size: cover;
+    margin: auto;
+    
+    
+}
+
+
+
+
+
+.budou div h2
+{
+  background-image: url("/budougarii.png");
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  background-size: cover;
+  margin: auto;
+  box-sizing: border-box;
+}
+
+.komugi div h2
+{
+  background-image: url("/susukii.png");
+  border-radius: 50%;
+  background-size: cover;
+  width: 100px;
+  height: 100px;
+  margin: auto;
+}
+ul.interview
+{
+
+  display: flex;
+  text-align: center;
+  overflow-x: scroll;
+  
+  
+
+}
+
+.interview
+{
+  text-align: center;
+  
+}
+
+li p 
+{
+  
+  margin-bottom: 180px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.eventimage
+{
+background-image: url("/event.jpg");
+padding-bottom: 30%;
+width: 100%;
+background-size: cover;
+
+}
+
+@media(max-width: 900px){
+.event
+{
+padding: 0 5vw;
+}
+
+}
+.event1 h2
+{
+
+  text-align: center;
+}
+
+.event1 p
+{
+  text-align: center;
+}
+
+
+.event1 .meta
+{
+  text-align: left;
+  padding: 40px;
+}
+
+
+div .info
+{
+
+  text-align: center;
+}
+
+div .mail
+{
+  margin-bottom: 5%;
+  background-color: #000000;
+  color: #fff;
+
+}
+
+.ichigo 
+{
+  width: 33.333%;
+  padding-right: 20px;
+  
+  
+}
+
+.budou
+{
+width: 33.333%;
+padding-right: 20px;
+
+}
+
+.komugi
+{
+  width: 33.333%;
+  padding-right: 20px
+}
+
+.ichigo h3 
+{
+  font-size: 20px
+}
+
+.budou h3 
+{
+  font-size: 20px
+}
+
+.komugi h3 
+{
+  font-size: 20px
+}
+
+
+
+  @media(max-width: 600px){
+  .ichigo p {
+  display: none;
+  }  
+  }
+
+
+@media(max-width: 600px){
+  .budou p {
+  display: none;
+  }  
+  }
+
+@media(max-width: 600px){
+  .komugi p {
+  display: none;
+  }  
+  }
+
+div .event01
+{
+
+  background-image: url("/event01.jpg");
+  padding-bottom: 20%;
+  width: 80%;
+  height: 0;
+  background-size: contain;
+  background-repeat: no-repeat;
+  margin-left: 20px
+
+  
+}
+
+div.event02
+{
+  background-image: url("/event02.jpg");
+  padding-bottom: 20%;
+  width: 80%;
+  height: 0;
+  background-size: contain;
+  background-repeat: no-repeat;
+  margin-left: 20px
+}
+div .event03 
+{
+background-image: url("/event01.jpg");
+  padding-bottom: 20%;
+  width: 80%;
+  height: 0;
+  background-size: contain;
+  background-repeat: no-repeat;
+  margin-left: 20px;
+  margin-right: 20px;
+
+}
+ul .event
+{
+  display: flex;
+  height: 100%
+
+
+}
+
+@media(max-width: 900px){
+div.event01 {
+    overflow-x: scroll;
+    
+}
+}
+
+@media(max-width: 600px){
+div.event02 {
+    overflow-x: scroll;
+}
+}
+
+@media(max-width: 600px){
+div.event03 {
+    overflow-x: scroll;
+}
+}
+
+div .info img
+{
+  
+}
+
+
+div .sns
+{
+background-color: #F2F2F2;
+margin-bottom: 60%;
+
+}
+
+
+
+div .facebook
+{
+background-image: url("/facebook.jpg");
+margin-bottom: 100%;
+color: #fff;
+background-size: cover;
+background-repeat: no-repeat;
+}
+
+
+div .overlay
+{
+background-image: url("/mainvisual.jpg");
+width: 100%;
+background-size: cover;
+background-repeat: no-repeat;
+border-radius: 0 0 20px 20px;
+}
+
+
+.google
+{
+  background-color: #000000;
+  color: #fff;
+  
+}
+
+.google a 
+{
+  color: #fff;
+}
+
+
+
+.google2
+{
+  background-color: #000000;
+  color: #fff;
+  
+}
+
+
+.google2 a 
+{
+  color: #fff;
+}
+
+ul .interview
+{
+width: 100%;
+overflow-x: scroll;
+padding: 0 5vw;
+}
+
+
+.container{
+max-width: 100%;
+padding-right: 0px;
+padding-left: 0px;
+
+}
+
+div .col-lg-8col-md-10 
+{
+padding: 0 0;
+}
+
+@media
+(min-width: 992px){
+.col-lg-8{max-width: 100%}
+}
+
+ul {
+
+  padding: 0px;
+}
+
+
+div .p
+{
+text-align: left;
+
+}
+
+.tennai
+{
+  display: flex;
+  width: 100%;
+}
+
+.tennai img
+{
+  width: 25%;
+}
+
+
+.car
+{
+text-align: left;
+padding: 40px;
+
+}
+
+.midori
+{
+text-align: left;
+padding: 40px;
+}
+
+.nagano
+{
+text-align: left;
+padding: 40px;
+}
+
+
+</style>
