b89d674d74183846d7cf2d6512d0c493b311c523
hoge <hina_lala@icloud.com>
2020-09-23 17:06:08 +0900

    nuxtjs_website

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+     
+
+   <!-- Main Content -->
+    <div class="container">
+    </div> 
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+a {
+    color: #212529;
+    transition: all 0.2s;
+  }
+
+  #mainNav {
+    position: absolute;
+    border-bottom: 1px solid #e9ecef;
+    background-color: white;
+    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  footer {
+    padding: 50px 0 65px;
+    text-align: center;
+  }
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,317 @@
+<template>
+  <div>
+    
+
+   
+      <div class="main"></div>
+      
+          
+            <div class="page-heading">
+              <div class="top"></div>
+              
+              
+            
+          
+        
+      </div>
+    
+
+    <!-- Main Content -->
+    
+    
+        
+          <div class="text">
+          <h1>日本でいちばん美味しい
+サンドイッチを目指して。</h1>
+          <p>私たちサンチは、丁寧に作り込んだ耳まで柔らかいパンと、信州の恵みを一身に受けたフルーツを使った、 地産地消にこだわるサンドイッチを作り続けています。</p>
+
+<p>目指すは〈日本でいちばん美味しいサンドイッチ〉。 お客様から「いちばんだね」と言ってもらうために、驚きと発見のあるサンドイッチを心掛けています。</p>
+
+<p>ずっしりと感じるサンドイッチの重みに驚き、たっぷり詰まった具材を眺めて笑顔になり、大きな口を開けて、あむっと頬張ると楽しい気持ちが湧き上がる。パンのむっちりとした食感が歯に伝わり、立て続けにフルーツや野菜を噛む爽快感で顔がほころぶ。</p>
+
+<p>もう一口。もう一口。最後の一口。
+
+驚き、楽しみ、感激、笑顔を生むことができるサンドイッチが、サンチにはあります。</p>
+          
+          </div>
+        <div class="flex1">
+        <img src="img1.jpg" width="100%">
+        <h1>サンドイッチが<br>街に人の流れを生む。</h1>
+       </div>
+
+       <div class="container">
+         <div class="flex2">
+         <img src="img4.jpg" width="200px">
+         <p>毎日食べても飽きない、誰からも愛されるサンドイッチが街にあること。 それが街づくりにつながると私たちは考えています。<br>
+        サンドイッチは、人の流れを作ります。
+美味しいサンドイッチを求めて街にさまざまな世代の人が集まれば、 それだけで活気に溢れます。</p>
+        </div>
+
+
+         <div class="flex3">
+           <p>そしてサンドイッチは、コミュニケーションを生み出します。 「美味しいね」「すごいね」「楽しいね」と言い合える文化を、 サンチのサンドイッチが実現し、豊かさと多幸感に溢れた街を作りたいと思っています。<br>
+
+サンドイッチが街にもたらすポジティブな影響を信じて、 私たちはサンチというブランドが、街の誇りとなるよう「美味しいサンドイッチ」を作り続けます。</p>
+<img src="img3.jpg" width="200px">
+
+          </div>
+
+         </div>
+
+          <div class="flex4">
+          <h2>シーンを広げるサンドイッチを。</h2>
+          <img src="img2.jpg" width="500px">
+          </div>
+
+        <div class="container">
+          <div class="flex5">
+          <p>いつでもどこでも楽しめるのがサンドイッチです。 それはお客様のシーンを広げることにもなります。<br>
+
+お気に入りの公園でかじりついても、家でゆっくり頬張っても、 パーティでワイワイつまんでもいい、 場所を選ばない自由がサンドイッチにはあります。</p>
+<img src="img5.jpg" width="200px">
+
+          </div>
+
+          <div class="flex6">
+          <img src="img6.jpg" width="200px">
+          <p>そしてもうひとつ、時間の自由も。<br>
+
+目覚めを幸せにする朝食としてのサンドイッチもあれば、 午後の活力になるランチのサンドイッチ、 夜の空腹を満たすサンドイッチもあります。<br>
+
+それぞれの人の思い思いの場所と時間に、 サンチのサンドイッチがいるワンシーンを思い描きながら、 美味しいサンドイッチを提供していきます。</p>
+          </div>
+
+        </div>
+          <img src="bottom.jpg" width="100%">
+
+
+          <p>© 2020 SANCH All Rights Reserved.</p>
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+    
+
+    <hr>
+
+    </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  a {
+    color: #212529;
+    transition: all 0.2s;
+  }
+
+  #mainNav {
+    position: absolute;
+    border-bottom: 1px solid #e9ecef;
+    background-color: white;
+    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  header.masthead {
+    margin-bottom: 50px;
+    background: no-repeat center center;
+    background-color: #868e96;
+    background-attachment: scroll;
+    position: relative;
+    background-size: cover;
+    text-align: center;
+    padding: 200px 0 150px;
+    color: white;
+  }
+
+  header.masthead .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: #212529;
+    opacity: 0.5;
+  }
+
+  footer {
+    padding: 50px 0 65px;
+    text-align: center;
+  }
+
+   div .text
+   {
+     text-align: center;
+
+   }
+
+  div .flex1
+  {
+    display: flex;
+    flex-wrap: wrap;
+    
+  }
+
+.flex1 img
+{
+max-width:100%;
+
+}
+
+.flex1 h1 
+{
+  font-size: 1.5rem;
+  padding-bottom: 0;
+  width: 100%;
+  line-height: 150%;
+  font-weight: bold;
+  
+
+}
+
+.mx-auto{
+  margin: 0
+}
+
+
+.flex2
+{
+  display: flex;
+   justify-content: space-between;
+   
+
+   
+
+}
+
+ .flex2 p
+ {
+   
+   line-height: 200%;
+ }
+
+ .flex2 img 
+ {
+   margin-right: 40px;
+   
+ }
+
+
+.flex3
+{
+  display: flex; 
+  justify-content: space-between;
+}
+
+
+.flex3 p
+{
+  margin-right: 40px;
+  line-height: 200%;
+}
+
+.flex4
+{
+  display: flex; 
+  justify-content: space-between;
+}
+
+.flex4 p
+{
+margin-right: 40px;
+  line-height: 200%;
+}
+
+
+
+.flex5
+{
+  display: flex;
+  justify-content: space-between;
+}
+
+.flex5 p
+{
+  line-height: 200%;
+}
+
+.flex5 img
+{margin-left: 40px;}
+
+.flex6
+{
+  display: flex;
+  justify-content: space-between;
+}
+
+
+.flex6 p
+{
+  line-height: 200%;
+  
+  
+}
+
+.flex6 img 
+{
+  margin-right: 40px;
+}
+
+.main
+{
+  background-image: url("/sub.jpg");
+  position: relative;
+  width: 100%;
+  justify-content: center;
+  padding-bottom: 400px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  
+  
+  
+}
+
+.flex1 
+{
+  margin-bottom: 20px;
+}
+
+.flex2
+{
+  margin-bottom: 20px;
+}
+
+.flex3 
+{
+  margin-bottom: 20px;
+}
+
+.flex4 
+{
+  margin-bottom: 20px;
+}
+
+.flex5 
+{
+  margin-bottom: 20px;
+}
+.flex6 
+{
+  margin-bottom: 20px;
+}
+
+
+
+</style>
